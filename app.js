87a26a1e1f332e90a6945d7bc23e07773b2f9e4a
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-11-01 19:06:01 -0300

    Testing server/client connectivity

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-(function() {
+
   function setup() {
     console.log('test')
   }
   // code to reference (taken from socket.io)
-  var socket = io('http://localhost:8080');
+  var socket = io('http://192.168.2.22:8080');
   socket.on('news', function (data) {
     console.log(data);
     socket.emit('my other event','jarret says hi back - message for server');
   });
-})()
+  function send(data) {
+    socket.emit('message', data)
+  }
