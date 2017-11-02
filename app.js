bbf64bea2a8ef6eb5fb4303eebce3bc2ece5c334
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-11-02 09:34:42 -0300

    Socket io now implemented with the chat elements

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
   last_animate = null
   last_from = null
-  function addMessageToChat(message, from) {
+  function addMessageToChat(from, message) {
     if (from == null) html = "<div class='message_full'><div class='message_body'><span class='message right'>" + message + "</span></div></div>"
     else if (last_from == from) html = "<div class='message_full'><div class='message_body'><span class='message left'>" + message + "</span></div></div>"
     else html = "<div class='message_full'><span class='name'>" + from + "</span><div class='message_body'><span class='message left'>" + message + "</span></div></div>"
@@ -8,19 +8,27 @@
     if (last_animate != null) last_animate.stop()
     $('.messages').append(html)
     last_animate = $(".messages").animate({
-      scrollTop: $('.messages').prop("scrollHeight")}, 1000, function(){
+      scrollTop: $('.messages').prop("scrollHeight")}, 400, function(){
           last_animate = null
       });
   }
-  // code to reference (taken from socket.io)
-  var socket = io('http://localhost:8080');
-  socket.on('message_update', function (data) {
-    console.log(data)
-    socket.emit('my other event','jarret says hi back - message for server');
+
+  // A $( document ).ready() block.
+  $(document).ready(function() {
+    $('.new_message input').on("keyup", function(e) {
+      if (e.keyCode == 13) {
+        send($('.new_message input').val())
+        $('.new_message input').val("")
+      }
+    });
   });
-  socket.on('servermessage', function(data) {
-    console.log(data)
-  })
+  // code to reference (taken from socket.io)
+  var socket = io('http://localhost:8080', { query: "user=jarret" }).connect();
+  socket.on('message_update', function (data) {
+    console.log('here')
+    addMessageToChat(data['user'], data['msg'])
+  });
   function send(data) {
+    addMessageToChat(null, data)
     socket.emit('message', data)
   }
