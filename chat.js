0f4cef8c952ee6ba6d9f7791e151d006f0ec0ba4
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-11-02 15:53:06 -0300

    Animations working properly

--- chat.js
+++ chat.js
@@ -1,6 +1,6 @@
 (function() {
   // socket.io implementation
-  var socket = io('http://192.168.0.17:8080', {
+  var socket = io('http://localhost:8080', {
     query: "user=jarret"
   })
 
--- main.js
+++ main.js
@@ -9,10 +9,11 @@
       if (!$('.send_icon').is(':visible')) {
           window.app.showSend()
       }
-      if (e.keyCode == 13) {
+      if ($(this).val() == "") window.app.hideSend()
+      else if (e.keyCode == 13) {
         window.app.send($('.new_message input').val())
+        window.app.forwardSend()
       }
-      if ($(this).val() == "") window.app.forwardSend()
     });
 
     $('.send_icon').on("click", function() {
